1921f25e49dfd11a493bd2d4d12e5fc7ebfc251a
dhruv465 <dhruv.465@gmail.com>
2024-07-23 12:58:40 +0530

    chore: Update CORS origin to use 'https://novu-spehere.vercel.app' for frontend URL

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: 'process.env.FRONTEND_URL',
+    origin: 'https://novu-spehere.vercel.app',
     credentials: true
 }))
 
